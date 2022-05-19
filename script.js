cf6d5f60b0d1a74c86555ff60bd4003714680ac5
cielyozora <49108900+cielyozora@users.noreply.github.com>
2022-05-19 02:17:43 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,120 @@
-const audioElem=document.querySelector("#audioElem");
-   const stopBtn=document.querySelector("#stopBtn");
-   const nextBtn=document.querySelector("#nextBtn");
-   const prevBtn=document.querySelector("#prevBtn");
-   const playPauseBtn=document.querySelector("#playPauseBtn");
-   const speakers=document.querySelectorAll(".speaker");
-   const clickSound=new Audio("https://raw.githubusercontent.com/Lucky-victory/zplayer/master/songs/click-sound.mp3");
-   const songs=["https://raw.githubusercontent.com/Lucky-victory/zplayer/master/songs/a-better-place-to-live.mp3","https://raw.githubusercontent.com/Lucky-victory/zplayer/master/songs/sing-me-to-sleep.mp3","https://raw.githubusercontent.com/Lucky-victory/zplayer/master/songs/angels-like-you.mp3"];
-   let songIndex=0;
-   function nextSong(){
-     clickSound.play();
- if(nextBtn.checked){
-     songIndex++;
-       songIndex >= songs.length ? songIndex=0 : "";
-       audioElem.src=songs[songIndex];
-             playAudio();
-
-       setTimeout(()=>{
-       nextBtn.checked=false
-     },500)
- }
-
-   }
-   
-      function prevSong(){
-       clickSound.play();
- if(prevBtn.checked){
-     songIndex--;
-       songIndex < 0 ? songIndex=songs.length-1 : "";
-      audioElem.src=songs[songIndex];
-      playAudio();
-     setTimeout(()=>{
-       prevBtn.checked=false;
-     },500)
- }
-   }
-   
-  function stopAudio(){
-     clickSound.play();
-    playPauseBtn.checked=false;
-    audioElem.currentTime=0;
-    playAudio();
-  }
-   function playAudio(){
-         clickSound.play();
-     let isChecked=playPauseBtn.checked;
-     if(isChecked && audioElem.paused){
-       stopBtn.checked=false;
-       audioElem.play();
-       for(let i=0;i < speakers.length;i++){
-         speakers[i].classList.add("pop");
-       }
-     }
-     else{
-       
-       audioElem.pause();
-            for(let i=0;i < speakers.length;i++){
-         speakers[i].classList.remove("pop");
-
-       }
-     }
-   }
-   audioElem.addEventListener("ended",stopAudio);
-   prevBtn.addEventListener("click",prevSong);
-   nextBtn.addEventListener("click",nextSong);
-   stopBtn.addEventListener("click",stopAudio);
-   playPauseBtn.addEventListener("click",playAudio);
+jQuery(function ($) {
+    'use strict'
+    var supportsAudio = !!document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        // initialize plyr
+        var player = new Plyr('#audio1', {
+            controls: [
+                'restart',
+                'play',
+                'progress',
+                'current-time',
+                'duration',
+                'mute',
+                'volume'
+            ]
+        });
+        // initialize playlist and controls
+        var index = 0,
+            playing = false,
+            mediaPath = 'https://node-28.zeno.fm/',
+            extension = '',
+            tracks = [{
+                "track": 1,
+                "name": "RPW VA ONE",
+                "duration": "Live",
+                "file": "mwhpvu78gg8uv?zs=P3Uxit8YSoOpbafV7jPzGA&rj-tok=AAABgNujWtgA9ziDUepBokb-bQ&rj-ttl=5"
+            }, {
+                "track": 2,
+                "name": "Sunday Classics (offline)",
+                "duration": "Live",
+                "file": ""
+            }],
+            buildPlaylist = $(tracks).each(function(key, value) {
+                var trackNumber = value.track,
+                    trackName = value.name,
+                    trackDuration = value.duration;
+                if (trackNumber.toString().length === 1) {
+                    trackNumber = '0' + trackNumber;
+                }
+                $('#plList').append('<li> \
+                    <div class="plItem"> \
+                        <span class="plNum">' + trackNumber + '.</span> \
+                        <span class="plTitle">' + trackName + '</span> \
+                        <span class="plLength">' + trackDuration + '</span> \
+                    </div> \
+                </li>');
+            }),
+            trackCount = tracks.length,
+            npAction = $('#npAction'),
+            npTitle = $('#npTitle'),
+            audio = $('#audio1').on('play', function () {
+                playing = true;
+                npAction.text('Now Playing...');
+            }).on('pause', function () {
+                playing = false;
+                npAction.text('Paused...');
+            }).on('ended', function () {
+                npAction.text('Paused...');
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    audio.play();
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }).get(0),
+            btnPrev = $('#btnPrev').on('click', function () {
+                if ((index - 1) > -1) {
+                    index--;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            btnNext = $('#btnNext').on('click', function () {
+                if ((index + 1) < trackCount) {
+                    index++;
+                    loadTrack(index);
+                    if (playing) {
+                        audio.play();
+                    }
+                } else {
+                    audio.pause();
+                    index = 0;
+                    loadTrack(index);
+                }
+            }),
+            li = $('#plList li').on('click', function () {
+                var id = parseInt($(this).index());
+                if (id !== index) {
+                    playTrack(id);
+                }
+            }),
+            loadTrack = function (id) {
+                $('.plSel').removeClass('plSel');
+                $('#plList li:eq(' + id + ')').addClass('plSel');
+                npTitle.text(tracks[id].name);
+                index = id;
+                audio.src = mediaPath + tracks[id].file + extension;
+            },
+            playTrack = function (id) {
+                loadTrack(id);
+                audio.play();
+            };
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+        loadTrack(index);
+    } else {
+        // boo hoo
+        $('.column').addClass('hidden');
+        var noSupport = $('#audio1').text();
+        $('.container').append('<p class="no-support">' + noSupport + '</p>');
+    }
+});
